08bbbe5073750638249cc5cb6e0077b70057a00f
keremkaragoz <108470789+keremkaragoz@users.noreply.github.com>
2022-07-29 17:21:55 +0300

    Add files via upload

---
+++ elmas.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class elmas {
+    public static void main(String[] args) {
+        Scanner klavye = new Scanner(System.in);
+        System.out.print("Bir Sayý Giriniz :");
+        int n = klavye.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k <= (n - i); k++)
+                System.out.print(" ");
+            for (int j = 1; j <= (2 * i) - 1; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+
+        for(int i=n-1;i>=1;i--)
+        {
+            for(int j=1;j<=n-i;j++)
+                System.out.print(" ");
+            for(int j=1;j<=2
+                    *i-1;j++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
